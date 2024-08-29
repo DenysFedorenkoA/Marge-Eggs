0935413852d0dc0db9e1d599471839bd7d9a240a
DenysFedorenkoA <121401812+DenysFedorenkoA@users.noreply.github.com>
2024-08-29 15:06:29 +0300

    Add files via upload

--- Marge Dino Eggs(1)/scripts/objRefTable.js
+++ Marge Dino Eggs(1)/scripts/objRefTable.js
@@ -123,7 +123,6 @@ self.C3_GetObjectRefTable = function () {
 		C3.Plugins.AJAX.Acts.Request,
 		C3.Plugins.Json.Acts.Parse,
 		C3.Plugins.AJAX.Exps.LastData,
-		C3.Plugins.TelegramMiniAppsSDK.Exps.CloudStorageValue,
 		C3.Plugins.Dictionary.Acts.JSONLoad,
 		C3.Plugins.Browser.Exps.Language,
 		C3.Plugins.AJAX.Cnds.OnComplete,
