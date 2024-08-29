c04c9ccf8d72d0637571181e22ae06084c3c40b0
DenysFedorenkoA <121401812+DenysFedorenkoA@users.noreply.github.com>
2024-08-29 15:39:12 +0300

    Add files via upload

--- Marge Dino Eggs(1)/scripts/c3runtime.js
+++ Marge Dino Eggs(1)/scripts/c3runtime.js
@@ -5649,6 +5649,7 @@ self.C3_GetObjectRefTable = function () {
 		C3.Plugins.AJAX.Acts.Request,
 		C3.Plugins.Json.Acts.Parse,
 		C3.Plugins.AJAX.Exps.LastData,
+		C3.Plugins.TelegramMiniAppsSDK.Exps.CloudStorageValue,
 		C3.Plugins.LocalStorage.Cnds.OnItemExists,
 		C3.Plugins.LocalStorage.Acts.GetItem,
 		C3.Plugins.LocalStorage.Cnds.OnItemGet,
@@ -6039,6 +6040,10 @@ self.C3_ExpressionFuncs = [
 		},
 		() => "en",
 		() => "load",
+		p => {
+			const f0 = p._GetNode(0).GetBoundMethod();
+			return () => f0("varible");
+		},
 		() => "loadGameObject",
 		() => "ads",
 		() => "leaderboard"
